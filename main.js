c733f89a8c047900e6310be5b42de93a25787fdb
luongmanh1604 <114908089+luongmanh1604@users.noreply.github.com>
2022-12-02 15:48:40 +0700

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ let classes = (classes) => document.getElementsByClassName(classes);
 let fullname = id("fullname"),
     password = id("password"),
     email = id("email"),
+    phone = id("phone"),
     errorMsg = classes("error"),
     successIcon = classes("success-icon"),
     failureIcon = classes("failure-icon");
@@ -12,8 +13,9 @@ let fullname = id("fullname"),
         e.preventDefault();
         engine(username, 0, "Ten dang nhap khong hop le");
         engine(email, 1, "Email khong hop le");
-        engine(password, 2, "mat khau khong hop le");
-        engine(password, 3,"Mat khau khong khop");
+        engine(password, 4, "mat khau khong hop le");
+        engine(password, 5,"Mat khau khong khop");
+        engine(phone, 2, "so dien thoai khong hop le")
     });
     let engine = (id, serial, message) => {
         if(id.value.trim()==="") {
