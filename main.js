b9bfa7335b050b1141e231e6fdefdb7bbd58b596
luongmanh1604 <114908089+luongmanh1604@users.noreply.github.com>
2022-12-02 17:17:11 +0700

    Update main.js

--- main.js
+++ main.js
@@ -5,17 +5,18 @@ let fullname = id("fullname"),
     password = id("password"),
     email = id("email"),
     phone = id("phone"),
+    password1 = id("password1"),
     errorMsg = classes("error"),
     successIcon = classes("success-icon"),
     failureIcon = classes("failure-icon");
    
     form.addEventListener("submit", (e) => {
         e.preventDefault();
-        engine(username, 0, "Ten dang nhap khong hop le");
+        engine(fullname, 0, "Ten dang nhap khong hop le");
         engine(email, 1, "Email khong hop le");
         engine(password, 4, "mat khau khong hop le");
-        engine(password, 5,"Mat khau khong khop");
-        engine(phone, 2, "so dien thoai khong hop le")
+        engine(password1, 5,"Mat khau khong khop");
+        engine(phone, 2, "so dien thoai khong hop le");
     });
     let engine = (id, serial, message) => {
         if(id.value.trim()==="") {
